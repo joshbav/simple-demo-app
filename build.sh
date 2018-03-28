476e4d725988d03934fc3962f7ecb3f318a70ce0
“joshbav” <josh@Joshs-Mac.local>
2018-03-28 08:38:06 -0700

    scripted commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+echo Uploading all files to github.com/joshbav/simple-demo-app
+echo
+# ALl files to automatically be added
+git add .
+git config user.name “joshbav” 
+git commit -m "scripted commit"
+git push -u origin master
+
